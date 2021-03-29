d9f0d8f3a652f8646d1e57dcd3b1ce123981720c
aiichiro <aiichirookuda@gmail.com>
2021-03-29 17:06:26 +0900

    FunctionComponents入門編を学びました。

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+type Foo = JSX.IntrinsicAttributes;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App message="Hello, React!" />
   </React.StrictMode>,
   document.getElementById('root')
 );
